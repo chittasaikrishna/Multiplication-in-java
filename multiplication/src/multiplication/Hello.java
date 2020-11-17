80949f176962c18b27255e3713fd9a1b68947d4e
Lenovo <stunninggamer123@gmail.com>
2020-11-17 11:52:47 +0530

    First Commit
    Uploding All files

---
+++ multiplication/src/multiplication/Hello.java
@@ -0,0 +1,23 @@
+package multiplication;
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    System.out.println("Enter number");
+	    int n = s.nextInt();
+	    int i;
+	    for(i=1; i<=10; i++ ) {
+	    	System.out.println(n+"*"+i +"="+n*i );
+	    	
+	    }
+	    	
+	    
+		
+		
+		
+
+	}
+
+}
